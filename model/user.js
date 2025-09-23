1bb9ad69a568c3f62f80806298692529dd4ecd70
ridwanullah <wetwan080@gmail.com>
2025-09-23 18:14:28 +0100

    build

--- model/user.js
+++ model/user.js
@@ -1,11 +1,9 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
 
 const User = mongoose.model("User", userSchema);
