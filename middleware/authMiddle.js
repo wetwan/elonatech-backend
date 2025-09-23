d5a569c6dc8d60a662f6a78bb207e31df2cc06b4
ridwanullah <wetwan080@gmail.com>
2025-09-23 15:41:49 +0100

    api working

---
+++ middleware/authMiddle.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+import User from "../model/userModel";
+
+export const protectUser = async (req, res, next) => {
+  const token = req.headers.token;
+  if (!token) {
+    return res.json({ success: false, message: "Not authorized, Login again" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SERECT);
+    req.user = await User.findById(decoded.id).select("-password");
+    next();
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
